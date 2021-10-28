1e7c411fb5c11a7cf5fd97788b42124120e003a6
Susie Stanley <susiestanley00@gmail.com>
2021-10-28 15:28:45 -0500

    updated journalform and journaleditform

--- src/components/journals/JournalForm.js
+++ src/components/journals/JournalForm.js
@@ -1,18 +1,116 @@
 //Author: Susie Stanley
 //Purpose: Creates and displays form for user to create a journal entry
 
-// import React, { useState, useEffect } from 'react'
-// import { update, getEntryById } from './JournalManager'
-// import { useParams, useHistory } from 'react-router-dom'
-import { WelcomeBar } from '../../components/navbar/WelcomeBar'
-import './Journal.css'
-import '../LifeHacker.css'
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { addJournal } from "./JournalManager";
+import { WelcomeBar } from "../../components/navbar/WelcomeBar";
+import "./Journal.css";
+import "../LifeHacker.css";
 
 export const JournalForm = () => {
-  // const [entry, setEntry] = useState({})
+  const [conflictDialog, setConflictDialog] = useState(false);
+
+  const [journal, setJournal] = useState({
+    title: "",
+    post: "",
+    dayTime: Date.now(),
+    userId: parseInt(sessionStorage.getItem("lifehacker_user")),
+  });
+
+  const history = useHistory();
+
+  const ResetForm = () => {
+    setJournal({
+      title: "",
+      post: "",
+      dayTime: Date.now(),
+      userId: parseInt(sessionStorage.getItem("lifehacker_user")),
+    });
+  };
+
+  const handleControlledInputChange = (event) => {
+    const newJournal = { ...journal };
+    let selectedVal = event.target.value;
+    // selectedVal = parseInt(selectedVal)
+    newJournal[event.target.id] = selectedVal;
+    setJournal(newJournal);
+  };
+
+  const handleSave = (event) => {
+    event.preventDefault(); //Prevents the browser from submitting the form
+    addJournal(journal).then(() => history.push("/journals"));
+    // }
+  };
+
   return (
-    <div className="page">
-      <WelcomeBar title="Add New Journal Entry" />
-    </div>
-  )
-}
+    <>
+      <div className="page">
+        <WelcomeBar title="Add New Journal Entry" />
+
+        <div className="form-flex">
+          <fieldset className="form">
+            <dialog className="dialog" open={conflictDialog}>
+              <div className="dialog-forms">
+                Please Fill In All Input Fields
+              </div>
+              <button
+                className="button-close"
+                onClick={(e) => setConflictDialog(false)}
+              >
+                Close
+              </button>
+            </dialog>
+
+            <div className="form__group">
+              <label htmlFor="title">Title: </label>
+              <input
+                type="text"
+                id="title"
+                onChange={handleControlledInputChange}
+                required
+                autoFocus
+                className="form__group--edit"
+                placeholder="Feeling awesome..."
+                value={journal.title}
+              />
+            </div>
+
+            <div className="form__group">
+              <label htmlFor="post">Entry: </label>
+              <input
+                type="text"
+                id="post"
+                onChange={handleControlledInputChange}
+                required
+                className="form__group--edit"
+                placeholder="Today I went to the..."
+                value={journal.post}
+              />
+            </div>
+          </fieldset>
+
+          <div className="form-btns">
+            <button type="button" className="form-btn" onClick={handleSave}>
+              Submit
+            </button>
+
+            <button type="button" className="form-btn" onClick={ResetForm}>
+              Reset Form
+            </button>
+
+            <button
+              type="button"
+              className="form-btn"
+              onClick={() => {
+                history.push("/journals");
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
