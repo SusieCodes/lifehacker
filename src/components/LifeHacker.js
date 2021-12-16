13c38e4500679c0bcb8e7e098dbf5fe73cb7a7b2
Susie Stanley <susiestanley00@gmail.com>
2021-12-16 14:36:23 -0600

    commented out applications views

--- src/components/LifeHacker.js
+++ src/components/LifeHacker.js
@@ -1,35 +1,35 @@
-import React from "react"
-import { Route, Redirect } from "react-router-dom"
-import { Sidebar }from './sidebar/Sidebar'
-import { ApplicationViews } from "./ApplicationViews"
-import { Login } from "../auth/Login"
-import { Register } from "../auth/Register"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { Sidebar } from "./sidebar/Sidebar";
+// import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "../auth/Login";
+import { Register } from "../auth/Register";
 
 export const LifeHacker = () => {
   return (
     <>
-    <Route
-      render={() => {
-        if (sessionStorage.getItem("lifehacker_user")) {
-          return (
-            <>
-              <Sidebar />
-              <ApplicationViews />
-            </>
-          )
-        } else {
-          return <Redirect to="/login" />
-        }
-      }}
-    />
+      <Route
+        render={() => {
+          if (sessionStorage.getItem("lifehacker_user")) {
+            return (
+              <>
+                <Sidebar />
+                {/* <ApplicationViews /> */}
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
 
-    <Route path="/login">
-      <Login />
-    </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
 
-    <Route path="/register">
-      <Register />
-    </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
     </>
-  )
-}
+  );
+};
