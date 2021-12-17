a00eb1fafd5b4bcaf9eb239dc48e1d4019e680b7
Susie Stanley <susiestanley00@gmail.com>
2021-12-17 09:29:35 -0600

    updated

--- src/components/LifeHacker.js
+++ src/components/LifeHacker.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { Sidebar } from "./sidebar/Sidebar";
-import { ApplicationViews } from "./ApplicationViews";
+// import { Sidebar } from "./sidebar/Sidebar";
+// import { ApplicationViews } from "./ApplicationViews";
 import { Login } from "../auth/Login";
 import { Register } from "../auth/Register";
 
