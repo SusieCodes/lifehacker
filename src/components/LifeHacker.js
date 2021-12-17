d16fa2a4f6b764c1b5a818d45d80bc9b7ae36920
Susie Stanley <susiestanley00@gmail.com>
2021-12-16 20:50:47 -0600

    added sidebar back

--- src/components/LifeHacker.js
+++ src/components/LifeHacker.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {
-  Route,
-  // Redirect
-} from "react-router-dom";
-// import { Sidebar } from "./sidebar/Sidebar";
+import { Route, Redirect } from "react-router-dom";
+import { Sidebar } from "./sidebar/Sidebar";
 // import { ApplicationViews } from "./ApplicationViews"
 import { Login } from "../auth/Login";
 import { Register } from "../auth/Register";
@@ -11,6 +8,21 @@ import { Register } from "../auth/Register";
 export const LifeHacker = () => {
   return (
     <>
+      <Route
+        render={() => {
+          if (sessionStorage.getItem("lifehacker_user")) {
+            return (
+              <>
+                <Sidebar />
+                {/* <ApplicationViews /> */}
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+
       <Route path="/login">
         <Login />
       </Route>
