6e6494a808e123f6fc7010d7d1f8826c4446cc0f
Susie Stanley <susiestanley00@gmail.com>
2021-12-17 09:18:29 -0600

    fixed routing on lifehacker.js

--- src/components/LifeHacker.js
+++ src/components/LifeHacker.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-// import { Sidebar } from "./sidebar/Sidebar";
-// import { ApplicationViews } from "./ApplicationViews"
+import { Sidebar } from "./sidebar/Sidebar";
+import { ApplicationViews } from "./ApplicationViews";
 import { Login } from "../auth/Login";
 import { Register } from "../auth/Register";
 
 export const LifeHacker = () => {
   return (
     <>
-      <Route
-        render={() => {
-          if (sessionStorage.getItem("lifehacker_user")) {
-            return <></>;
-          } else {
-            return <Redirect to="/login" />;
-          }
-        }}
-      />
+      <Route path="/">
+        {sessionStorage.getItem("lifehacker_user") ? (
+          <>
+            <Sidebar />
+            <ApplicationViews />
+          </>
+        ) : (
+          <Redirect to="/login" />
+        )}
+      </Route>
 
       <Route path="/login">
         <Login />
